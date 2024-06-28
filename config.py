2b66b388a90b20211733cdc947e1c9730c10d6fe
wishvishnu179 <137043572+wishvishnu179@users.noreply.github.com>
2024-06-28 14:59:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -75,10 +75,9 @@ Exᴀᴍᴩʟᴇ: <code> /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
     SEND_METADATA = """
 ❪ SET CUSTOM METADATA ❫
 
-<b>☞ Fᴏʀ Exᴀᴍᴘʟᴇ :-</b>
-◦ <code><b> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By :- @TGCinemaworld" -metadata author="@TGCinemaworld" -metadata:s:s title="Subtitled By :- @TGCinemaworld" -metadata:s:a title="Telegram - @TGCinemaworld" -metadata:s:v title="Telegram - @TGCinemaworld</b>" </code>
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:
 
-<b>📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @TGCinemaworld</b>
+◦ <code>Telegram : @TGCinemaworld</code>
 """
 
     PROGRESS_BAR = """<b>\n
