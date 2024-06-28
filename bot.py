e04b5371ad4d521b51432194215471e57512ab50
wishvishnu179 <137043572+wishvishnu179@users.noreply.github.com>
2024-06-28 13:53:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="SnowRenamer",
+            name="Metadata Rename bot",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
