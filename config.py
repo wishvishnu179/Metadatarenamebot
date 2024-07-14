c2f94c1451a36614849a6533e88b67cae12c017b
wishvishnu179 <137043572+wishvishnu179@users.noreply.github.com>
2024-07-14 10:07:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,24 +39,35 @@ class Config(object):
 
 class Txt(object):
     # part of text configuration
-    START_TXT = """<b>Hᴀɪ {} 👋,
-Tʜɪs Is Aɴ Aᴅᴠᴀɴᴄᴇᴅ Aɴᴅ Yᴇᴛ Pᴏᴡᴇʀꜰᴜʟ Rᴇɴᴀᴍᴇ Bᴏᴛ
-Usɪɴɢ Tʜɪs Bᴏᴛ Yᴏᴜ Cᴀɴ Rᴇɴᴀᴍᴇ & Cʜᴀɴɢᴇ Tʜᴜᴍʙɴᴀɪʟ Oꜰ Yᴏᴜʀ Fɪʟᴇ
-Yᴏᴜ Cᴀɴ Aʟsᴏ Cᴏɴᴠᴇʀᴛ Vɪᴅᴇᴏ Tᴏ Fɪʟᴇ & Fɪʟᴇ Tᴏ Vɪᴅᴇᴏ
-Tʜɪs Bᴏᴛ Aʟꜱᴏ Sᴜᴘᴘᴏʀᴛs Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Aɴᴅ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ</b>
+    START_TXT = """<b><i>Hello {} 👋 
+
+➻ This Is An Advanced And Yet Powerful Rename Bot.
+
+➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.
+
+➻ You Can Also Convert Video To File And File To Video.
+
+➻ This Bot Also Supports Custom Thumbnail And Custom Caption.</i></b>
+
+<b>Bot Is Made By :</b> @Vishnudhfm14
 """
 
     ABOUT_TXT = """<b>╭───────────⍟
-• ᴍy ɴᴀᴍᴇ : {}
-• ᴜᴘᴅᴀᴛᴇꜱ : <a href=https://t.me/TGCinemaworld>TGCinemaworld</a>
-╰───────────────⍟ """
+├🤖 My Nᴀᴍᴇ : {}
+├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : <a href=https://t.me/Vishnudhfm14>Vɪsʜɴᴜ MB🤍</a>
+├☃️ Fᴏᴜɴᴅᴇʀ Oꜰ : <a href=https://t.me/TGCinemaworld>Jᴏɪɴ Hᴇʀᴇ</a>
+├📕 Lɪʙʀᴀʀy : <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
+├✏️ Lᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pyᴛʜᴏɴ 3</a>
+├💾 Dᴀᴛᴀ Bᴀꜱᴇ: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
+├🌀 Mʏ Sᴇʀᴠᴇʀ : <a href=https://dashboard.render.com>Rᴇɴᴅᴇʀ</a>
+╰───────────────⍟ </b>"""
 
     HELP_TXT = """
-🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
+🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴᴀɪʟ</u></b>
   
-<b>•></b> /start Tʜᴇ Bᴏᴛ Aɴᴅ Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
-<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
-<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /start Tʜᴇ Bᴏᴛ Aɴᴅ Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴᴀɪʟ.
+<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴᴀɪʟ.
+<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ.
 
 
 📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
@@ -69,7 +80,7 @@ Exᴀᴍᴩʟᴇ: <code> /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
 ⏰ Dᴜʀᴀᴛɪᴏɴ: {duration} </code>
 
 ✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
-<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nᴀᴍᴇ \nAɴᴅ Sᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].
 """
 
     SEND_METADATA = """
@@ -77,15 +88,23 @@ Exᴀᴍᴩʟᴇ: <code> /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
 
 <b>☞ Fᴏʀ Exᴀᴍᴘʟᴇ :-</b>
 
-◦ <code><b> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By :- @TGCinemaworld" -metadata author="@TGCinemaworld" -metadata:s:s title="Subtitled By :- @TGCinemaworld" -metadata:s:a title="Telegram - @TGCinemaworld" -metadata:s:v title="Telegram - @TGCinemaworld</b>" </code>
+◦ <code><b> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="𝙑𝙄𝙎𝙃𝙉𝙐👽@𝐓𝐆𝐂𝐢𝐧𝐞𝐦𝐚𝐰𝐨𝐫𝐥𝐝" -metadata author="𝙑𝙄𝙎𝙃𝙉𝙐👽@𝐓𝐆𝐂𝐢𝐧𝐞𝐦𝐚𝐰𝐨𝐫𝐥𝐝" -metadata:s:s title="Subs By :-𝐕𝐢𝐬𝐡𝐧𝐮 𝐌𝐁🤍" -metadata:s:a title="𝙑𝙄𝙎𝙃𝙉𝙐👽@𝐓𝐆𝐂𝐢𝐧𝐞𝐦𝐚𝐰𝐨𝐫𝐥𝐝" -metadata:s:v title="𝙑𝙄𝙎𝙃𝙉𝙐👽@𝐓𝐆𝐂𝐢𝐧𝐞𝐦𝐚𝐰𝐨𝐫𝐥𝐝" </code>
 
-<b>📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @TGCinemaworld</b>
+<b><i>📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Vishnudhfm14</b></i>
 """
 
     PROGRESS_BAR = """<b>\n
-╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱
-🗃️ sɪᴢᴇ: {1} | {2}
-⏳️ ᴅᴏɴᴇ : {0}%
-🚀 sᴘᴇᴇᴅ: {3}/s
-⏰️ ᴇᴛᴀ: {4}
-╰━━━━━━━━━━━━━━━ </b>"""
+╭━━━━❰ Pʀᴏɢʀᴇss Bᴀʀ ❱━➣
+┣⪼ 📁 Sɪᴢᴇ : {1} | {2}
+┣⪼ ⏳️ Dᴏɴᴇ : {0}%
+┣⪼ 🚀 Sᴩᴇᴇᴅ : {3}/s
+┣⪼ ⏰️ Eᴛᴀ : {4}
+╰━━━━━━━━━━━━━━━➣ </b>"""
+
+    DONATE_TXT = """
+<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+
+<b>🛍 UPI ID:</b> `wishvishnu179-1@okaxis`
+"""
